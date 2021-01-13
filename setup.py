47408ea9b80d30b229c2edcbde1fcb62eb378beb
Dhyey <dhyeythumar@gmail.com>
2021-01-13 21:03:39 +0530

    add setup config

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="Video Streamer",
+    version="1.0",
+    py_modules=['videostreamer'],
+    url="https://github.com/Dhyeythumar/video-streamer",
+    author="Dhyey Thumar",
+    author_email="dhyeythumar@gmail.com",
+    description="Capture a video using the virtual screen in Google Colab & broadcast the live stream to youtube/twitch.",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
