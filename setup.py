10da2024bc455ec94e09833cff77779455695634
dhyey <dhyeythumar@gmail.com>
2021-06-20 14:38:36 +0530

    refactor: update py pkg setup

--- setup.py
+++ setup.py
@@ -1,16 +1,40 @@
 from setuptools import setup
+import mlagents_video_streamer
+import os
+
+description = "Live stream Unity's ML-Agents training process from Google Colab to Twitch/YouTube server."
+
+README_path = os.path.join(os.path.abspath(
+    os.path.dirname(__file__)), 'README.md')
+if os.path.exists(README_path):
+    with open(README_path, encoding='utf-8') as f:
+        long_description = f.read()
+    long_description_content_type = 'text/markdown'
+else:
+    print("No Readme.md")
+    long_description = description
+    long_description_content_type = 'text/plain'
+
+VERSION = mlagents_video_streamer.__version__
 
 setup(
-    name="Video Streamer",
-    version="1.0",
-    py_modules=['videostreamer'],
-    url="https://github.com/Dhyeythumar/video-streamer",
+    name="mlagents-video-streamer",
     author="Dhyey Thumar",
     author_email="dhyeythumar@gmail.com",
-    description="Capture a video using the virtual screen in Google Colab & broadcast the live stream to youtube/twitch.",
+    version=VERSION,
+    packages=["mlagents_video_streamer"],
+
+    url="https://github.com/dhyeythumar/mlagents-video-streamer",
+    description=description,
+    long_description=long_description,
+    long_description_content_type=long_description_content_type,
+
+    keywords=["ML-Agents", "Video Streamer", "Unity Engine", "Google Colab"],
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ]
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries"],
 )
